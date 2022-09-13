a53c91c39b8a789d7d5b21e5dd403cd8d1604c58
vladimir <slr-klesks-300@yandex.ru>
2022-09-13 23:06:35 +1000

    Задача2

---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт 
+// максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int Prompt(string messege)
+{
+    Console.Write(messege);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int isNumber1 = Prompt("Введите число 1 > ");
+int isNumber2 = Prompt("Введите число 2 > ");
+int isNumber3 = Prompt("Введите число 3 > ");
+int max = isNumber1;
+
+if (isNumber2 > max)
+{
+    max = isNumber2;
+}
+
+if (isNumber3 > max)
+{
+    max = isNumber3;
+}
+
+Console.WriteLine("Максимальное число -> " + max);
