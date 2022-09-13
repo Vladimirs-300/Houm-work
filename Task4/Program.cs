fb7bd0b1c594d4b66db1182f20d173f03f7baa09
vladimir <slr-klesks-300@yandex.ru>
2022-09-14 00:40:14 +1000

    задача4

---
+++ Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает
+// все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+int Prompt(string messege)
+{
+    Console.Write(messege);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int Number = Prompt(" Введите число > ");
+int i = 1;
+System.Console.WriteLine(" Чётные числа от 1 до " + Number);
+while (i <= Number)
+{
+    if (i % 2 != 1)
+    {
+        System.Console.Write(i + " ");
+    }
+    i++;
+}
