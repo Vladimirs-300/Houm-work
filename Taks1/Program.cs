39ea41a3b510a0798a4f0164f0d3aaf8e5bea669
vladimir <slr-klesks-300@yandex.ru>
2022-09-13 22:52:25 +1000

    задача_1

---
+++ Taks1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int Prompt(string messege)
+{
+    Console.Write(messege);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int isNumberA = Prompt("Введите число > ");
+int isNumberB = Prompt("Введите число > ");
+
+if (isNumberA > isNumberB)
+{
+    Console.WriteLine("Первое число " + isNumberA + " больше чем второе " + isNumberB);
+}
+else
+{
+    Console.WriteLine("Второе число " + isNumberB + " больше чем первое " + isNumberA);
+}
+
